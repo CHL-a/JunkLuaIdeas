5d03ea4c1d959ffd8c451514865726ed8ffccf19
CHL_a <48702697+CHL-a@users.noreply.github.com>
2023-01-20 10:44:04 -0800

    13.1

--- Stuff/ResponsiveQueue.lua
+++ Stuff/ResponsiveQueue.lua
@@ -1,38 +1,51 @@
+-- SPEC
+export type object<params...> = {
+	iterationFunc: (params...) -> nil;
+	queue: {};
+	thread: thread;
+	enqueue: (self: object<params...>, params...) -> nil;
+}
+
+-- CLASS
 local ResponsiveQueue = {}
+ResponsiveQueue.__index = ResponsiveQueue
 
-function ResponsiveQueue.new<rValues..., params...>(func: (params...) -> rValues...)
-	local object = {}
-	object.queue = {}
-
-	object.thread = coroutine.create(function()
+function new<params...>(f: (params...) -> nil): object<params...>
+	local result = setmetatable({}, ResponsiveQueue)
+	local result: object<params...> = result
+	
+	result.queue = {}
+	result.iterationFunc = f
+	result.thread = coroutine.create(function()
 		while true do
-			if #object.queue == 0 then 
+			if #result.queue == 0 then 
 				coroutine.yield()
 			end
 
-			local val = table.remove(object.queue, 1)
-
-			func(unpack(val))
+			local val = table.remove(result.queue, 1)
+			local b = result.iterationFunc
+			
+			b(unpack(val))
 		end
 	end)
 	
-	object.call = function(...: params...)
-		local temp = {}
-		
-		for i = 1, select('#', ...) do
-			temp[i] = select(i, ...)
-		end
-		
-		table.insert(object.queue, temp)
-		
-		if #object.queue == 1 then
-			coroutine.resume(object.thread)
-		end
+	coroutine.resume(result.thread)
+	
+	return result
+end
+ResponsiveQueue.new = new
+ResponsiveQueue.enqueue = function<p...>(self: object<p...>, ...: p...)
+	local a = {}
+	
+	for b = 1, select('#', ...) do
+		a[b] = select(b, ...)
 	end
 	
+	table.insert(self.queue, a)
 	
-	return object
+	if #self.queue == 1 then
+		coroutine.resume(self.thread)
+	end
 end
-
 
 return ResponsiveQueue
