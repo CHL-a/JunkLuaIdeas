2046fd575d573c0a8166c6b4e875c92583ffffd0
CHL_a <48702697+CHL-a@users.noreply.github.com>
2023-01-23 08:43:16 -0800

    14.1

--- Stuff/LuaUClass.lua
+++ Stuff/LuaUClass.lua
@@ -4,10 +4,27 @@ export type subclass<super> = super & {
 
 local Class = {}
 
-function inherit<A>(t: A): subclass<A>
+function inherit<A>(t: A, methods): subclass<A>
 	local a = t
 	local result = table.clone(a)
 	result.__super = t;
+	
+	-- metatable evaluation
+	local mt = getmetatable(result)
+	
+	if mt and methods then
+		mt = table.clone(mt)
+		
+		local cloneIndex = table.clone(mt.__index)
+		
+		mt.__index = cloneIndex
+		
+		for i, v in next, methods do
+			cloneIndex[i] = v
+		end
+		
+		setmetatable(result, mt)
+	end
 	
 	return result
 end
