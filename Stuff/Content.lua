b2852bf2a9c4b6cf7bcb369a89f2d17f1cde78be
CHL_a <48702697+CHL-a@users.noreply.github.com>
2023-01-27 21:55:42 -0800

    16

---
+++ Stuff/Content.lua
@@ -0,0 +1,21 @@
+-- SPEC
+export type protocol = 'rbxassetid' |
+	'rbxasset' |
+	'rbxthumb' |
+	'rbxhttp' |
+	'https' |
+	'http'
+
+-- CLASS
+local Content = {}
+local ContentProvider = game.ContentProvider
+
+Content.contentProvider = ContentProvider
+
+Content.getComponents = function(s: string) : (protocol?, string?)
+	local p, st = s:match('(.-)://(.*)')
+	
+	return p, st
+end
+
+return Content
