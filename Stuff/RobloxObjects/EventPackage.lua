b7a8ae1ff66cc23e5a3f2e4362c93e55fd07c294
CHL_a <48702697+CHL-a@users.noreply.github.com>
2023-07-10 08:37:28 -0700

    38.2

--- Stuff/RobloxObjects/EventPackage.lua
+++ Stuff/RobloxObjects/EventPackage.lua
@@ -14,16 +14,15 @@ type structFunc<a...> = (a...) -> any?;
 export type eventSubjectTypes = 'connect' | 'once' | 'wait'
 
 -- object when establishing a connection
-export type connection = {
-	disconnect: () -> nil;
-}
+type __connection<a...> = {
+	__q: queue<a...>;
+	__id: number;
+	__f: structFunc<a...>;
+	
+	disconnect: (self: __connection<a...>) -> nil;
+} & RBXScriptConnection
 
--- event object
-export type event<a...> = {
-	connect: (responder: structFunc<a...>) -> connection;
-	once: (responder: structFunc<a...>) -> connection;
-	wait: () -> a...
-}
+export type connection<a...> = __connection<a...>
 
 type queue<a...> = {
 	{
@@ -33,114 +32,131 @@ type queue<a...> = {
 	}
 };
 
--- main object
-export type eventPackage<a...> = {
-	id: number;
-	event: event<a...>;
-	queue: queue<a...>;
+-- event object
+type __event<a...> = {
+	__availibleId: number;
+	__queue: queue<a...>;
 
-	insert: (eventSubjectTypes, structFunc<a...>) -> nil;
-	fire: (a...) -> nil;
+	__insert: (self:event<a...>, eventSubjectTypes, structFunc<a...>) -> nil;
+	connect: (self: __event<a...>, responder: structFunc<a...>) -> __connection<a...>;
+	once: (self: __event<a...>,responder: structFunc<a...>) -> __connection<a...>;
+	wait: (self: __event<a...>) -> a...
+} & RBXScriptSignal
+
+export type event<a...> = __event<a...>
+
+
+-- main object
+type __package<a...> = {
+	event: event<a...>;
+
+	fire: (self:__package<a...>, a...) -> nil;
 }
 
+export type package<a...> = __package<a...>
+
 -- CLASS
-local EventPackage = {}
+local LuaUTypes = require(script.Parent.LuaUTypes)
 
+-- connection
+local connection = {}
+connection.__index = connection
 
-function EventPackage.new<a...>()
-	local object: eventPackage<a...>
-	local event: event<a...>
-
-	-- object states
-	local queue: queue<a...> = {}
-
-	-- methods
-	local function insert(ev: eventSubjectTypes, f: structFunc<a...>) 
-		table.insert(queue, {
-			subject = ev;
-			f = f;
-			id = object.id
-		})
-	end
-
-	local function fire(...: a...)
-		local i = 1;
-
-		while i <= #queue do
-			local v = queue[i]
-
-			-- v.f(...)
-			-- ^ bruh
-
-			local f: structFunc<a...> = v.f
-			f(...)
-
-			if v.subject ~= 'connect' then
-				table.remove(queue, i)
-				i -= 1
-			end
-
-			i += 1
-		end
-	end
-
-	-- event
-
-	-- event methods
-	local function createConnection(f, id)
-		local result: connection = {
-			disconnect = function()
-				for i, v in next, queue do
-					if v.f == f and v.id == id then
-						table.remove(queue, i)
-						return
-					end
-				end
-			end,
-		}
-
-		return result
-	end
-
-	local function getConnector(eventType: eventSubjectTypes)
-		return function(f: structFunc<a...>)
-			object.id += 1
-			insert(eventType, f)
-
-			return createConnection(f,object.id)
-		end
-	end
-
-	local function eWait(): a...
-		object.id += 1
-
-		local thread = coroutine.running()
-
-		object.insert('wait', function(...: a...)
-			coroutine.resume(thread, ...)
-		end)
-
-		return coroutine.yield(thread)
-	end
-
-	event = {
-		connect = getConnector('connect');
-		once = getConnector('once');
-		wait = eWait;
-	}
-
-	object = {
-		id = 0;
-		queue = queue;
-
-		event = event;
-		fire = fire,
-		insert = insert
-	}
-
+connection.new = function<a...>(f: structFunc<a...>, id: number, q: queue<a...>)
+	local object: __connection<a...> = LuaUTypes.disguise(setmetatable({}, connection))
+	object.__id = id;
+	object.__f = f
+	object.__q = q
+	
 	return object
 end
 
+connection.disconnect = function<a...>(self:__connection<a...>)
+	for i, v in next, self.__q do
+		if v.f == self.__f and v.id == self.__id then
+			table.remove(self.__q, i)
+			return
+		end
+	end
+end
+connection.Disconnect = connection.disconnect
 
+-- event
+local event = {}
+event.__index = event
+event.new = function<a...>()
+	local object: __event<a...> = LuaUTypes.disguise(setmetatable({}, event))
+	object.__availibleId = 0;
+	object.__queue = {}
+	
+	return object
+end
 
-return EventPackage
+event.ConnectParallel = function()error('unimplemented')end
+
+event.__insert = function<a...>(self: __event<a...>, ev, f)
+	self.__availibleId += 1
+	table.insert(self.__queue, {
+		subject = ev;
+		f = f;
+		id = self.__availibleId
+	})
+end
+
+event.connect = function<a...>(self: __event<a...>, f)
+	self:__insert('connect', f)
+	
+	return connection.new(f, self.__availibleId, self.__queue)
+end
+event.Connect = event.connect
+
+event.wait = function<a...>(self:__event<a...>)
+	local thread = coroutine.running()
+	
+	self:__insert('wait', function(...)
+		coroutine.resume(thread,...)
+	end)
+	
+	return coroutine.yield(thread)
+end
+event.Wait = event.wait
+
+event.once = function<a...>(self:__event<a...>, f)
+	self:__insert('once', f)
+
+	return connection.new(f, self.__availibleId, self.__queue)
+end
+event.Once = event.once
+
+-- package
+local package = {}
+package.__index = package
+
+package.new = function<a...>()
+	local object: __package<a...> = LuaUTypes.disguise(setmetatable({}, package))
+	local event: __event<a...> = event.new()
+	
+	object.event = event
+	
+	return object
+end
+
+package.fire = function<a...>(self:__package<a...>, ...:a...)
+	local i = 1
+	local q = self.event.__queue
+	
+	while i <= #q do
+		local v = q[i]
+		
+		v.f(...)
+		
+		if v.subject ~= 'connect' then
+			table.remove(q, i)
+			i -= 1
+		end
+		
+		i += 1
+	end
+end
+
+return package
