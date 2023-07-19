1d9d8f32e39fbb8b71b7842d0febc740fa961df2
CHL_a <48702697+CHL-a@users.noreply.github.com>
2023-07-18 18:38:13 -0700

    39.1

--- Stuff/RobloxObjects/Class.lua
+++ Stuff/RobloxObjects/Class.lua
@@ -1,31 +1,48 @@
-export type subclass<super> = super & {
-	__super: super;
+type __subclass<super> = super & {
+	__supers: {super};
 }
+
+export type subclass<A> = __subclass<A>
 
 local Class = {}
 
-function inherit<A>(t: A, methods): subclass<A>
-	local a = t
-	local result = table.clone(a)
-	result.__super = t;
+local disguise = function<A>(x): A return x end
+
+function getLatestFunction<A>(self: __subclass<A>, i: string)
+	assert(i ~= '__supers', 'self missing index __supers')
+	
+	for j = #self.__supers, 1, -1 do
+		local onIndex = disguise(self.__supers[j]) 
+		
+		if type(onIndex) == 'function'then return onIndex(self, i)end
+		assert(type(onIndex) == 'table')
+		
+		local method = onIndex[i] 
+		if not method then continue;end
+		
+		return method
+	end
+	
+	print(self.__supers)
+	error(`Missing method: {i}`)
+end
+
+function inherit<A>(t: A, methods): __subclass<A>
+	local result: __subclass<A> = disguise(t)
+	result.__supers = result.__supers or {}
 
 	-- metatable evaluation
-	local mt = getmetatable(result)
-
-	if methods then
-		if mt then
-			mt = table.clone(mt)
-
-			local cloneIndex = mt.__index and table.clone(mt.__index) or {}
-
-			mt.__index = cloneIndex
-
-			for i, v in next, methods do
-				cloneIndex[i] = v
-			end
+	local metatable = getmetatable(disguise(result))
+	
+	if methods and metatable then
+		if metatable.__index ~= getLatestFunction then
+			table.insert(result.__supers, metatable.__index)
+			metatable.__index = getLatestFunction
 		end
-
-		setmetatable(result, mt or methods)
+		
+		table.insert(result.__supers, methods)
+		
+		setmetatable(disguise(result), metatable)
 	end
 
 	return result
