00cfb5d776844b692824b180a60c3732bc222e41
CHL_a <48702697+CHL-a@users.noreply.github.com>
2023-07-21 17:59:30 -0700

    41.4

--- Stuff/RobloxObjects/Class.lua
+++ Stuff/RobloxObjects/Class.lua
@@ -1,5 +1,6 @@
 type __subclass<super> = super & {
 	__supers: {super};
+	__super: super;
 }
 
 export type subclass<A> = __subclass<A>
@@ -10,19 +11,19 @@ local disguise = function<A>(x): A return x end
 
 function getLatestFunction<A>(self: __subclass<A>, i: string)
 	assert(i ~= '__supers', 'self missing index __supers')
-	
+
 	for j = #self.__supers, 1, -1 do
 		local onIndex = disguise(self.__supers[j]) 
-		
+
 		if type(onIndex) == 'function'then return onIndex(self, i)end
 		assert(type(onIndex) == 'table')
-		
+
 		local method = onIndex[i] 
 		if not method then continue;end
-		
+
 		return method
 	end
-	
+
 	print(self.__supers)
 	error(`Missing method: {i}`)
 end
@@ -33,17 +34,19 @@ function inherit<A>(t: A, methods): __subclass<A>
 
 	-- metatable evaluation
 	local metatable = getmetatable(disguise(result))
-	
+
 	if methods and metatable then
 		if metatable.__index ~= getLatestFunction then
 			table.insert(result.__supers, metatable.__index)
 			metatable.__index = getLatestFunction
 		end
-		
+
 		table.insert(result.__supers, methods)
-		
+
 		setmetatable(disguise(result), metatable)
 	end
+	
+	result.__super = result.__supers[#result.__supers - 1]
 
 	return result
 end
