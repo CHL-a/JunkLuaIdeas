874e8eca911432eac9dcabb9805177943240da3a
CHL_a <48702697+CHL-a@users.noreply.github.com>
2023-12-01 22:03:39 -0800

    type test
    subjected to change

--- Stuff/RobloxObjects/LuaUTypes.lua
+++ Stuff/RobloxObjects/LuaUTypes.lua
@@ -1,13 +1,15 @@
 local module = {}
 
 function same(x)return x end
+function disguise<A>(x: any) : A return same(x) end
 
-module.assertify = function<A>(val: A?): A
-	return same(val)
-end
+module.assertify = function<A>(val: A?): A return same(val)end
+module.disguise = disguise
 
-module.disguise = function<A>(val: any): A
-	return same(val)
-end
+--// !!! subjected to change because this way is terrible because intersection operation 
+--// conflicts with metatmethods
+export type __legacyObject<metamethods> = typeof(
+	setmetatable({},disguise{} :: metamethods)
+)
 
 return module
