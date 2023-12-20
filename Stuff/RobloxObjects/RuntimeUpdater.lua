45473358149ff3b3bf2a6c137b64ab8f484e1db5
CHL_a <48702697+CHL-a@users.noreply.github.com>
2023-12-19 19:01:43 -0800

    Update RuntimeUpdater.lua

--- Stuff/RobloxObjects/RuntimeUpdater.lua
+++ Stuff/RobloxObjects/RuntimeUpdater.lua
@@ -7,14 +7,19 @@ local Dash = require(Objects.Dash) :: DashInterface.module
 type __updatable = {
 	canUpdate: boolean;
 	shouldDisconnect: boolean;
+	updatablePriority: number?;
+	
 	update: (self:__updatable, delta: number) -> nil;
 }
 export type updatable = __updatable
 type __object = {
-	updateThread:thread;
-	collection: {[__updatable]: true};
+	updateThread: thread;
+	indexCollection: {number};
+	collection: {{[__updatable]: true}};
+	
+	insertIndex: (self:__object, number) -> nil;
 	commence: (self:__object) -> nil;
-	addObject: (self:__object, __updatable) -> nil;
+	addObject: (self:__object, __updatable, priority: number?) -> nil;
 	removeObject: (self:__object, __updatable) -> nil;
 	update: (self:__object, delta: number) -> nil;
 	getUpdatables: (self:__object) -> {__updatable};
@@ -28,20 +33,53 @@ abstract.__index = abstract
 abstract.new = function()
 	local self: __object = disguise(setmetatable({}, abstract))
 	self.collection = {}
+	self.indexCollection = {}
 	
 	return self
 end
 
 abstract.commence = Class.abstractMethod
-abstract.addObject = function(self:__object, u:__updatable)self.collection[u] = true; end
-abstract.removeObject = function(self:__object, u:__updatable)self.collection[u] = nil; end
-abstract.getUpdatables = function(self:__object)return Dash.keys(self.collection)end
+abstract.insertIndex = function(self:__object, i: number)
+	for j, v in next, self.indexCollection do
+		if v > i then continue end
+		if v == i then return end;
+		
+		table.insert(self.indexCollection, j, i)
+		
+		return
+	end
+	
+	table.insert(self.indexCollection, i)
+end
+
+abstract.addObject = function(self:__object, u:__updatable, p: number?)
+	local p = p or u.updatablePriority or 1
+	self:insertIndex(p)
+	local collection = self.collection
+	local map = collection[p] or {}
+	
+	if not collection[p] then
+		collection[p] = map
+	end
+	
+	map[u] = true;
+end
+abstract.removeObject = function(self:__object, u:__updatable)
+	local map = self.collection[u.updatablePriority or 1]
+	if not map then return end;
+	map[u] = nil;
+end
+abstract.getUpdatables = function(self:__object)
+	return Dash.keys(Dash.flat(self.collection))
+end
 
 abstract.update = function(self:__object, delta: number)
-	for u in next, self.collection do
-		if u.shouldDisconnect then self:removeObject(u)continue;end
-		if not u.canUpdate then continue end;
-		u:update(delta)
+	for _, v in next, self.indexCollection do
+		for u in next, self.collection[v] do
+			if u.shouldDisconnect then self:removeObject(u)continue;end
+			if not u.canUpdate then continue end;
+			u:update(delta)
+		end
 	end
 end
 
