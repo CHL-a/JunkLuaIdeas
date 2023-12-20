33370731fab8c9e0b7525f26242a0b801fe30bbc
CHL_a <48702697+CHL-a@users.noreply.github.com>
2023-12-20 15:59:15 -0800

    Update Math.lua

--- Stuff/RobloxObjects/Math.lua
+++ Stuff/RobloxObjects/Math.lua
@@ -1,7 +1,9 @@
 local module = {}
 
-module.isNan = function(x: number)return x ~= x end
+module.epsilon = 9E-323
 
+module.isNan = function(x: number)return x ~= x end
 module.isInfinity = function(x:number)return x+1==x;end
+module.oneDLerp = function(i: number, f:number, a: number)return i + (f - i) * a end
 
 return module
