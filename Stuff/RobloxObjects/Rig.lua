ca1acd71bf9db3b0cd841a621be2091e992f196b
CHL_a <48702697+CHL-a@users.noreply.github.com>
2023-12-23 14:56:28 -0800

    Create Rig.lua

---
+++ Stuff/RobloxObjects/Rig.lua
@@ -0,0 +1,49 @@
+--// TYPE
+type __object = {
+	character: Model;
+	humanoid: Humanoid;
+	__constructorArg: __constructorArgs;
+	
+	findChild: <A>(self:__object, name: string) -> A?;
+	waitChild: <A>(self:__object, name: string) -> A;
+	__getChildFromArg: <A>(self:__object, name: string) -> A;
+}
+export type object = __object
+
+type __constructorArgs = {
+	shouldYield: boolean?;
+}
+export type constructorArgs = __constructorArgs
+
+type __anyFn = (...any) -> ...any
+
+-- CLASS
+local Rig = {}
+Rig.__index = Rig
+
+function disguise<A...>(...: any): A...return...end
+--local disguise = require(sc)
+
+function Rig.new(char: Model, arg: __constructorArgs): __object
+	local self: __object = disguise(setmetatable({}, Rig))
+	self.character = char
+	self.__constructorArg = arg
+	
+	-- mind this
+	self.humanoid = self:__getChildFromArg('Humanoid')
+	
+	return self
+end
+
+Rig.findChild = function(self:__object, n: string)return self.character:FindFirstChild(n)end
+Rig.waitChild = function(self:__object, n: string)return self.character:WaitForChild(n)end
+
+Rig.__getChildFromArg = function(self:__object, n: string)
+	return assert(
+		self:findChild(n) or self.__constructorArg.shouldYield and self:waitChild(n),
+		`Missing child: {n}`
+	)
+end
+
+
+return Rig
