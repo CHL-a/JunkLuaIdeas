e6a0f1ba4d9ce1fa81e60066c83d6dde1e75708c
CHL_a <48702697+CHL-a@users.noreply.github.com>
2023-12-31 15:07:49 -0800

    Create IKCollection.lua

---
+++ Stuff/RobloxObjects/IKCollection.lua
@@ -0,0 +1,35 @@
+--// TYPE
+type __object = {
+	collection: {IKControl};
+	
+	enable: (self: __object, isEnabled: boolean) -> nil;
+	add: (self:__object, ...IKControl) -> nil;
+}
+export type object = __object
+
+--// MAIN
+local module = {}
+local disguise = require(script.Parent.LuaUTypes).disguise
+local TableUtils = require(script.Parent['@CHL/TableUtils'])
+
+module.__index = module
+
+function module.new(...: IKControl)
+	local self: __object = disguise(setmetatable({}, module))
+	
+	self.collection = {...}
+	
+	return self;
+end
+
+module.enable = function(self: __object, isEnabled: boolean)
+	for _, v in next, self.collection do
+		v.Enabled = isEnabled
+	end
+end
+
+module.add = function(self: __object, ...: IKControl)
+	TableUtils.push(self.collection, ...)
+end
+
+return module
