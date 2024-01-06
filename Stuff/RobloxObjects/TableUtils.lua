4c1c803a2775da2177836544e7eb6ee477338998
CHL_a <48702697+CHL-a@users.noreply.github.com>
2024-01-05 21:04:02 -0800

    Update TableUtils.lua

--- Stuff/RobloxObjects/TableUtils.lua
+++ Stuff/RobloxObjects/TableUtils.lua
@@ -2,7 +2,9 @@ local module = {}
 local LuaUTypes = require(script.Parent.LuaUTypes)
 local disguise = LuaUTypes.disguise
 
-function module.deepSoftIndex<A, B>(t: {[A]: any}, ...: A): B?
+
+
+function deepSoftIndex<A, B>(t: {[A]: any}, ...: A): B?
 	for i = 1, select('#', ...) do
 		local index = select(i, ...)
 		t = t[index]
@@ -13,15 +15,15 @@ function module.deepSoftIndex<A, B>(t: {[A]: any}, ...: A): B?
 	return disguise(t);
 end
 
-function module.safeSet(t: any, i: any, v: any): (boolean, string?)
+function safeSet(t: any, i: any, v: any): (boolean, string?)
 	return pcall(function()
 		t[i] = v
 	end)
 end
 
-function module.imprint<A>(t: A, t2: {[any]: any}, shouldWarn: boolean?): A
+function imprint<A>(t: A, t2: {[any]: any}, shouldWarn: boolean?): A
 	for i, v in next, t2 do
-		local s, e = module.safeSet(t, i, v)
+		local s, e = safeSet(t, i, v)
 		if shouldWarn and not s then
 			warn(e)
 		end
@@ -30,7 +32,7 @@ function module.imprint<A>(t: A, t2: {[any]: any}, shouldWarn: boolean?): A
 	return t
 end
 
-function module.push<A, B>(t: A, ...: B): A
+function push<A, B>(t: A, ...: B): A
 	for i = 1, select('#', ...) do
 		local a = select(i, ...)
 		table.insert(disguise(t), a)
@@ -38,7 +40,7 @@ function module.push<A, B>(t: A, ...: B): A
 	return t
 end
 
-function module.clearNils<A>(t: A): A
+function clearNils<A>(t: A): A
 	local i = 1
 	local b = disguise(t)
 
@@ -47,11 +49,37 @@ function module.clearNils<A>(t: A): A
 			table.remove(b,i)
 			i -= 1
 		end
-
 		i += 1
 	end
 
 	return t
 end
+
+function fill<A>(ground: A, concrete: A): A
+	local a, b = disguise(ground, concrete)
+	
+	for i, v in next, b do
+		if a[i] == nil then
+			a[i] = v
+		end
+	end
+	
+	return ground
+end
+
+function defaultify<A>(a: A?, default: A): A
+	if not a then return default end
+	
+	fill(a, default)
+	
+	return a
+end
+
+module.deepSoftIndex = deepSoftIndex
+module.safeSet = safeSet
+module.imprint = imprint
+module.push = push
+module.clearNils = clearNils
+module.fill = fill
 
 return module
