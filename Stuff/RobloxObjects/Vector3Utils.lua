4570f974445afcb7e66362eac2d8d1d907a0b052
CHL_a <48702697+CHL-a@users.noreply.github.com>
2024-01-08 22:23:43 -0800

    Update Vector3Utils.lua

--- Stuff/RobloxObjects/Vector3Utils.lua
+++ Stuff/RobloxObjects/Vector3Utils.lua
@@ -6,4 +6,21 @@ module.getRelativeVector = function(cf: CFrame, world: Vector3)
 		cf.LookVector * world.Z
 end
 
+type __worldPositionObject = BasePart | Attachment
+export type worldPositionObject = __worldPositionObject
+
+module.getAbsolutePosition = function(v: __worldPositionObject): Vector3
+	if typeof(v) == 'Instance' then
+		if v:IsA('BasePart') then
+			return v.Position
+		elseif v:IsA('Attachment') then
+			return v.WorldPosition
+		else
+			error(`Unknown v {v} of class {v.ClassName} `)
+		end
+	else
+		error(`Unknown type v of {v} of type {typeof(v)}`)
+	end
+end
+
 return module
