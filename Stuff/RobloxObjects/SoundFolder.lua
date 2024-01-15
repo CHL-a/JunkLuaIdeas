ee543b6d53caa949d13e26f401295b1d71933a94
CHL_a <48702697+CHL-a@users.noreply.github.com>
2024-01-14 16:44:20 -0800

    Create SoundFolder.lua

---
+++ Stuff/RobloxObjects/SoundFolder.lua
@@ -0,0 +1,31 @@
+--// TYPES
+type __object = {
+	folder: Folder;
+	getSound: (self: __object) -> Sound;
+}
+export type object = __object
+
+--// MAIN
+local module = {}
+local Objects = script.Parent
+local LuaUTypes = require(Objects.LuaUTypes)
+
+disguise = LuaUTypes.disguise
+
+module.__index = module
+
+function module.new(folder: Folder)
+	local self: __object = disguise(setmetatable({}, module))
+	
+	self.folder = folder
+	
+	return self
+end
+
+module.getSound = function(self: __object, name: string)
+	local s = self.folder:FindFirstChild(name)
+	
+	return assert(s, `Missing a sound: {name}`)
+end
+
+return module
