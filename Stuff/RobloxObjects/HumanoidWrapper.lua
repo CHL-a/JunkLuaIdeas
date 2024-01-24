acf7d0dbee4725f6b53387be72a6fde34387f6da
CHL_a <48702697+CHL-a@users.noreply.github.com>
2024-01-23 20:55:23 -0800

    Create HumanoidWrapper.lua

---
+++ Stuff/RobloxObjects/HumanoidWrapper.lua
@@ -0,0 +1,29 @@
+--// TYPES
+local Objects = script.Parent
+
+type __object = {
+	humanoid: Humanoid;
+	isWalking: (self: __object) -> boolean;
+}
+
+--// MAIN
+local module = {}
+local LuaUTypes = require(Objects.LuaUTypes)
+
+disguise = LuaUTypes.disguise
+module.__index = module
+
+function module.new(h: Humanoid)
+	local self: __object = disguise(setmetatable({}, module))
+	self.humanoid = h
+	return self
+end
+
+module.isWalking = function(self: __object)
+	local h = self.humanoid
+	return h.MoveDirection.Magnitude > .5 and 
+		h:GetState()==Enum.HumanoidStateType.Running and
+		h.FloorMaterial ~= Enum.Material.Air
+end
+
+return module
