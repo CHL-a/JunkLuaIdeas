5f753782d3365f3a9beb6c07e541c679e8de9c92
CHL_a <48702697+CHL-a@users.noreply.github.com>
2024-02-13 21:25:10 -0800

    Update DestructableInterface.lua

--- Stuff/RobloxInterfaces/DestructableInterface.lua
+++ Stuff/RobloxInterfaces/DestructableInterface.lua
@@ -1,6 +1,7 @@
 type __object = {
 	isDestroyed: boolean;
 	destroy: (self: __object) -> nil;
+	Destroy: (self: __object) -> nil;
 	assertDestruction: (self: __object) -> nil;
 }
 export type object = __object
@@ -8,10 +9,12 @@ export type object = __object
 local module = {}
 
 module.destroy = function(self: __object)self.isDestroyed = true;end
+module.Destroy = module.destroy
 module.assertDestruction = function(self: __object)
 	assert(
 		not self.isDestroyed, 
 		'Attempting to destroy a destroyed object.'
 	)
 end
+
 return module
