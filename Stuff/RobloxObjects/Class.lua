9109c0d889dabde166909715cf0e7fc14c5e849f
CHL_a <48702697+CHL-a@users.noreply.github.com>
2024-02-13 21:14:46 -0800

    Update Class.lua

--- Stuff/RobloxObjects/Class.lua
+++ Stuff/RobloxObjects/Class.lua
@@ -45,7 +45,12 @@ type __proxy<A> = {
 
 --// MAIN
 local Class = {}
-local disguise = function<A>(x): A return x end
+local Objects = script.Parent
+local LuaUTypes = require(Objects.LuaUTypes)
+
+disguise = LuaUTypes.disguise
+find = table.find
+clone = table.clone
 
 local Method = {}
 
@@ -67,15 +72,11 @@ Proxy.__index = function<A>(self: __proxy<A>, i: string)
 	-- self index
 	local self_val = rawget(self,i) or Proxy[i]
 
-	if self_val ~= nil then
-		return self_val
-	end
+	if self_val ~= nil then return self_val end
 
 	-- self.__object methods
 	local method = self:__get_method(i)
-	if method then
-		return method
-	end
+	if method then return method end
 
 	-- self.__object regular values
 	local val = rawget(disguise(self.__object),i)
@@ -96,7 +97,7 @@ Proxy.new = function<A>(object: A, class)
 end
 Proxy.__get_class_i = function<A>(self: __proxy<A>)
 	return assert(
-		table.find(
+		find(
 			rawget(
 				disguise(self).__object, 
 				'__supers'
@@ -115,8 +116,6 @@ Proxy.__get_super_class = function<A>(self:__proxy<A>)
 			return class
 		end
 	end
-
-	return nil
 end
 Proxy.__get_method = function<A>(self:__proxy<A>,name: string)
 	local supers = disguise(self.__object).__supers
@@ -127,7 +126,6 @@ Proxy.__get_method = function<A>(self:__proxy<A>,name: string)
 	local m
 	for i = start, 1, -1 do
 		local superclass = supers[i]
-
 		if type(superclass) ~= 'table' then continue end;
 		
 		local fn = superclass[name]
@@ -174,6 +172,10 @@ Method.new = function<A>(object: A, func: __function, class:__class?,name: strin
 	return self
 end
 
+--##################################################################################
+--##################################################################################
+--##################################################################################
+
 function getLatestFunction<A>(self: __subclass<A>, i: string)
 	if i == '__supers' and not rawget(self,'__supers') then return end
 
@@ -181,9 +183,7 @@ function getLatestFunction<A>(self: __subclass<A>, i: string)
 	local first = supers[#supers]
 
 	-- first method always returned raw
-	if type(first) == 'table' and first[i] then
-		return first[i]
-	end
+	if type(first) == 'table' and first[i] then return first[i]end
 
 	-- upper class methods returned psuedo
 	for j = #supers - 1, 1, -1 do
@@ -202,36 +202,105 @@ function getLatestFunction<A>(self: __subclass<A>, i: string)
 	end
 end
 
+function other(s: string)
+	return function<A>(self: subclass<A>, ...)
+		local fn = disguise(self)[s]
+		
+		if not fn then
+			error(`Attempting to use operation on uninoperable object: {s}`)
+		end
+		
+		return fn(self, ...)
+	end
+end
+
+mainMeta = {__index = getLatestFunction}
+specialMeta = ('__mode,__metatable,__newindex,__tostring,__index'):split(',')
+overwritableMeta = ('__newindex,__tostring'):split(',')
+
+for _, v in next,LuaUTypes.metamethods do
+	if find(specialMeta, v) then continue end
+	
+	mainMeta[v] = other(v)
+end
+
 function inherit<A, B>(t: A, methods, is_debugging): B-- __subclass<A>
-	local result: __subclass<A> = disguise(t)
+	local _t = disguise(t)
+	local result: __subclass<A> = disguise(_t)
+	
 	local supers = rawget(result,'__supers') or {}
 	rawset(result,"__supers", supers)
 
 	-- metatable evaluation
-	local old_metatable = getmetatable(disguise(result))
+	local metat = getmetatable(disguise(result))
 	
-	if not old_metatable then old_metatable = {}
-	else old_metatable = table.clone(old_metatable)
-	end
+	local new_metat
 	
-	if not old_metatable.__index or
-		old_metatable.__index ~= getLatestFunction then
-		if old_metatable.__index then
-			table.insert(supers, old_metatable.__index)
+	if not metat or metat.__index ~= getLatestFunction then
+		new_metat = mainMeta
+		
+		if metat then
+			local didClone = false
+			
+			for _, v in next, overwritableMeta do
+				if not metat[v] then continue end
+				
+				if not didClone then
+					new_metat = clone(new_metat)
+					didClone = true
+				end
+				
+				new_metat[v] = other(v)
+			end
 		end
-		old_metatable.__index = getLatestFunction
 	end
+	
+	table.insert(supers, metat.__index)
+	
+	--[[  ] ]
+	if metat ~= mainMeta then
+		if metat and metat.__index then
+			table.insert(supers, metat.__index)
+		end
+		
+		setmetatable(_t, mainMeta)
+	end
+	--]]
+	
+	--[[] ]
+	if not metat then metat = {}
+	else metat = table.clone(metat)
+	end
+	
+	if not metat.__index or
+		-- establish __index
+		metat.__index ~= getLatestFunction then
+		if metat.__index then
+			table.insert(supers, metat.__index)
+		end
+		metat.__index = getLatestFunction
+		
+		-- establish others
+		for _, v in next, LuaUTypes.metamethods do
+			for i = #supers, 1, -1 do
+				if typeof(supers[i][v]) ~= 'function'then continue; end
+				
+				metat[v] = extraneousMetamethod(v)
+				break
+			end
+		end
+	end
+	--]]
 	
 	if methods then
 		table.insert(supers, methods)
 	end
 
-	setmetatable(disguise(result), old_metatable) -- do something here later?
+	setmetatable(disguise(result), new_metat) -- do something here later?
 
 	-- set up self.__super
 	for i = #supers - 1, 1, -1 do
 		local class = supers[i]
-
 
 		if type(class) == 'table'then
 			local super = Proxy.new(result, class)
@@ -256,10 +325,14 @@ end
 
 function hasClass(obj, class)
 	return isClass(obj, class) or 
-		obj.__supers and not not table.find(obj.__supers, class)
+		obj.__supers and not not find(obj.__supers, class)
 end
 
 function getErrorFunc(s: string) return function() error(s) end end
+
+--########################################################################################
+--########################################################################################
+--########################################################################################
 
 Class.inherit = inherit
 Class.isClass = isClass
