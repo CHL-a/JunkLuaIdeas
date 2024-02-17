35c3f1388b2e233a43a8bcd70bbc1d231983d35b
CHL_a <48702697+CHL-a@users.noreply.github.com>
2024-02-16 18:51:41 -0800

    Update Math.lua

--- Stuff/RobloxObjects/Math.lua
+++ Stuff/RobloxObjects/Math.lua
@@ -1,9 +1,28 @@
-local module = {}
+module = {}
+
+function isNan(x: number)return x ~= x end
+function isInfinity(x:number)return x+1==x;end
+function oneDLerp(i: number, f:number, a: number)return i + (f - i) * a end
+
+function getDigit(n: number, base: number?, i: number)
+	return (n // ((base or 10) ^ i)) % (base or 10)
+end
+
+function getDigits(n: number, base: number?)
+	local result = 1
+
+	if n > 0 then
+		result += math.log(n, base) // 1
+	end
+
+	return result
+end
 
 module.epsilon = 9E-323
-
-module.isNan = function(x: number)return x ~= x end
-module.isInfinity = function(x:number)return x+1==x;end
-module.oneDLerp = function(i: number, f:number, a: number)return i + (f - i) * a end
+module.isNan = isNan
+module.isInfinity = isInfinity
+module.oneDLerp = oneDLerp
+module.getDigit = getDigit
+module.getDigits = getDigits
 
 return module
