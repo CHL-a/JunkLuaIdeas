3ee061dbb4a8ed9b7bd05c2d717c533dcb5569a6
CHL_a <48702697+CHL-a@users.noreply.github.com>
2024-03-29 13:51:10 -0700

    Update CharacterRig.lua

--- Stuff/RobloxObjects/CharacterRig.lua
+++ Stuff/RobloxObjects/CharacterRig.lua
@@ -1,7 +1,8 @@
 -- SPEC
 local Objects = script.Parent
+
 local RigBase = require(Objects.RigBase)
-local Class = require(Objects.Class)
+local Class = require(Objects.Class);
 
 type __object = {
 	humanoidRootPart: Part;
@@ -14,17 +15,20 @@ export type constructorArgs = __constructorArgs
 
 -- CLASS
 local CharacterRig = {}
-local disguise = require(Objects.LuaUTypes).disguise
 
-CharacterRig.__index = CharacterRig
+disguise = require(Objects.LuaUTypes).disguise
 
 CharacterRig.new = function(char:Model, arg: __constructorArgs?): object
-	local self: __object = disguise(Class.inherit(RigBase.new(char, arg), CharacterRig))
+	local self: __object = RigBase.new(char, arg):__inherit(CharacterRig)
+		--disguise(Class.inherit(RigBase.new(char, arg), CharacterRig))
 	
 	self:__setLimbFromConstruction('Head')
 	self:__setLimbFromConstruction('HumanoidRootPart')
 	
 	return self
 end
+
+CharacterRig.__index = CharacterRig
+CharacterRig.className = 'CharacterRig'
 
 return CharacterRig
