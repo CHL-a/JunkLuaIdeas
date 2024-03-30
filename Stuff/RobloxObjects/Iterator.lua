53d6dde6a3caedc899520d0dc9e8a43417dcd414
CHL_a <48702697+CHL-a@users.noreply.github.com>
2024-03-29 18:53:53 -0700

    Update Iterator.lua

--- Stuff/RobloxObjects/Iterator.lua
+++ Stuff/RobloxObjects/Iterator.lua
@@ -1,29 +1,34 @@
 --// TYPES
-local Class = require(script.Parent.Class)
+local Objects = script.Parent
 
-type __object<A...> = {
-	proceed: (self:__object<A...>) -> A...;
-	canProceed: (self:__object<A...>) -> boolean;
-}
-export type object<A...> = __object<A...>
+local Object = require(Objects.Object)
+local Class = require(Objects.Class)
+
+export type object<A...> = {
+	proceed: (self: object<A...>) -> A...;
+	canProceed: (self: object<A...>) -> boolean;
+	call: (self: object<A...>) -> A...;
+} & Class.subclass<Object.object>
 
 --// MAIN
 local module = {}
-local disguise = require(script.Parent.LuaUTypes).disguise
 
-module.__index = module
+disguise = require(Objects.LuaUTypes).disguise
 
-module.__call = function<A...>(self: __object<A...>)
+function module.new<A...>(): object<A...>
+	return Object.new():__inherit(module)
+		--disguise(setmetatable({}, module)) :: __object<A...>
+end
+
+module.call = function<A...>(self: object<A...>)
 	if self:canProceed() then
 		return self:proceed()
 	end
 end
 
-module.new = function<A...>()
-	return disguise(setmetatable({}, module)) :: __object<A...>
-end
-
 module.proceed = Class.abstractMethod
 module.canProceed = Class.abstractMethod
+module.__index = module
+module.className = '@CHL/Iterator'
 
 return module
