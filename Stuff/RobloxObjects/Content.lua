cddb7299ed34ff58c99e8d1e5ea0d6db0149a066
CHL_a <48702697+CHL-a@users.noreply.github.com>
2024-04-03 09:54:16 -0700

    Update Content.lua

--- Stuff/RobloxObjects/Content.lua
+++ Stuff/RobloxObjects/Content.lua
@@ -1,21 +1,42 @@
--- SPEC
-export type protocol = 'rbxassetid' |
-	'rbxasset' |
-	'rbxthumb' |
-	'rbxhttp' |
-	'https' |
+-- TYPES
+local Objects = script.Parent
+
+local Object = require(Objects.Object)
+local Class = require(Objects.Class)
+
+export type protocol = 'rbxassetid' | 'rbxasset' | 'rbxthumb' | 'rbxhttp' | 'https' | 
 	'http'
 
--- CLASS
-local Content = {}
-local ContentProvider = game:GetService('ContentProvider')
+export type object = {
+	protocol: protocol;
+	suffix: string;
+	
+	toString: (self: object) -> string;
+} & Class.subclass<Object.object>
 
-Content.contentProvider = ContentProvider
+-- MAIN
+local module = {}
 
-Content.getComponents = function(s: string) : (protocol?, string?)
-	local p, st = s:match('(.-)://(.*)')
+from = {}
 
-	return p, st
+function module.new(protocol: protocol, suffix: string)
+	local self: object = Object.new():__inherit(module)
+	
+	self.protocol = protocol
+	self.suffix = suffix
+	
+	return self
 end
 
-return Content
+function from.singleString(s: string)
+	local p, s = s:match('(%l+)://(.*)')
+	return module.new(p, s)
+end
+
+module.toString = function(self: object)return `{self.protocol}://{self.suffix}`end
+
+module.from = from
+module.__index = module
+module.className = '@CHL/Content'
+
+return module
