96a026fdc0ffa8e3d9f3e62a00b24decd93174b6
CHL_a <48702697+CHL-a@users.noreply.github.com>
2024-08-07 14:21:05 -0700

    Create ReceiptLadder.lua

---
+++ Stuff/RobloxObjects/ReceiptLadder.lua
@@ -0,0 +1,40 @@
+local Objects = game:GetService('ReplicatedStorage').Objects
+local Class = require(Objects.Class)
+local LadderFunction = require(Objects["@CHL/LadderFunction"])
+
+export type receipt = {
+	CurrencySpent: number;
+	PlaceIdWherePurchased: number;
+	PlayerId: number;
+	ProductId: number;
+	PurchaseId: string;
+}
+export type object = Class.subclass<LadderFunction.object<receipt>>
+
+Ladder = {}
+
+function Ladder.new(): object return LadderFunction.Ladder.new():__inherit(Ladder)end
+
+function Ladder.call(self: object, reciept: receipt)
+	for i, v in next, self.rungs do
+		local results = {v(reciept)}
+		
+		local value = results[1]
+		
+		if value == 'continue' then continue;
+		elseif value == 'end' then return unpack(results, 2)
+		elseif typeof(value) == 'EnumItem' 
+			and value.EnumType == Enum.ProductPurchaseDecision then return value
+		else
+			error(`Attempting to climb ladder with invalid return: Expected: rung_type\z
+				, got: {results[1]}, at i={i}`)
+		end
+	end
+end
+
+Ladder.__index = Ladder
+Ladder.className = '@CHL/ReceiptLadder'
+
+local module = {}
+
+return module
