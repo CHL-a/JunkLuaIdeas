19456a36e316c2c525c5ffbb3b2848c9fce4e8a0
CHL_a <48702697+CHL-a@users.noreply.github.com>
2024-09-04 19:00:03 -0700

    Update SpringInterface.lua

--- Stuff/RobloxInterfaces/SpringInterface.lua
+++ Stuff/RobloxInterfaces/SpringInterface.lua
@@ -6,27 +6,29 @@ export type nevermore<A> = {
 	Value: A;
 	Position: A;
 	p: A;
-	
+
 	-- velocity
 	Velocity: A;
 	v: A;
-	
+
 	-- target
 	Target: A;
 	t: A;
-	
+
 	-- damper
 	Damper: number;
 	d: number;
-	
+
 	-- speed
 	Speed: number;
 	s: number;
-	
+
 	-- clock method
 	Clock: () -> number;
-	
+
 	-- methods
 	Impulse: (self:nevermore<A>, velocity: A) -> ();
 	TimeSkip: (self:nevermore<A>, delta: number) -> ();
 }
+
+return true
