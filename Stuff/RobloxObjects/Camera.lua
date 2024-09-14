d8d261a5f34c800d52523ac44a7a2a07a6de5e97
CHL_a <48702697+CHL-a@users.noreply.github.com>
2024-09-14 09:20:50 -0700

    Update Camera.lua

--- Stuff/RobloxObjects/Camera.lua
+++ Stuff/RobloxObjects/Camera.lua
@@ -21,10 +21,10 @@ type enum_item<U> = CNum.enum_item<U>
 
 local FOVUpdateMode: CNum.enum<
 	'Default' | 'Spring',
-	{
-		Default: enum_item<'Default'>;
-		Spring: enum_item<'Spring'>;
-	}
+{
+	Default: enum_item<'Default'>;
+	Spring: enum_item<'Spring'>;
+}
 	> = CNum.new{'Default', 'Spring'}
 
 export type FOVUpdateMode = typeof(FOVUpdateMode.union)
@@ -33,19 +33,23 @@ export type FOVUpdateMode = typeof(FOVUpdateMode.union)
 --#######################################################################################
 --#######################################################################################
 
+local HRay = require(Objects["@CHL/HeuristicRay"])
+
 export type mode = Enum.CameraType 
-	| 'AttachOnPart'
+| 'AttachOnPart'
 
 export type object = {
 	camera: Camera;
 	mode: mode;
 	fov_update_mode: enum_item<FOVUpdateMode>;
 	fov_spring: Spring.object<number>;
+	h_ray: HRay.object;
 	
 	changeMode: (self: object, m: mode) -> ();
 	changeFOVUpdateMode: (self: object, m: FOVUpdateMode)->();
+	get_target: (self: object, to: Vector3, filter: HRay.filter_heuristic?)->();
 } & Object.object_inheritance
-  & RuntimeUpdater.updatable
+& RuntimeUpdater.updatable
 
 -- MAIN
 local module = {}
@@ -54,12 +58,13 @@ disguise = require(Objects.LuaUTypes).disguise
 
 function module.new(c: Camera): object
 	local self: object = Object.from.class(module)
-	
+
 	self.camera = c;
 	self.mode = c.CameraType
 	self.fov_update_mode = FOVUpdateMode.enum_items.Default
 	self.canUpdate = true
-	
+	self.h_ray = HRay.new(Vector3.zero, Vector3.zero)
+
 	return self
 end
 
@@ -67,9 +72,9 @@ function module.changeFOVUpdateMode(self: object, m: FOVUpdateMode | enum_item<F
 	if type(m) == 'string' then
 		m = FOVUpdateMode.enum_items[m]
 	end
-	
+
 	if self.fov_update_mode == m then return; end
-	
+
 	self.fov_update_mode = disguise(m)
 	self.fov_spring.t = self.camera.FieldOfView
 	self.fov_spring.p = self.camera.FieldOfView
@@ -77,32 +82,41 @@ end
 
 function module.changeMode(self: object, m: mode)
 	local old = self.mode
-	
+
 	if old == m then return end
-	
+
 	self.mode = m
-	
+
 	if m == 'AttachOnPart' then
 		self.camera.CameraType = Enum.CameraType.Scriptable
 		return
 	end
-	
+
 	self.camera.CameraType = m
 end
 
 function module.update(self: object, dt: number)
 	if self.mode == 'AttachOnPart' then
 		local subject = self.camera.CameraSubject
-		
+
 		if (subject and subject:IsA('BasePart')) then 
 			self.camera.CFrame = subject.CFrame
 		end
 	end
-	
+
 	if self.fov_update_mode == FOVUpdateMode.enum_items.Spring then
 		self.fov_spring:update(dt)
 		self.camera.FieldOfView = self.fov_spring.p
 	end
+end
+
+function module.get_target(self: object, to: Vector3, filter: HRay.filter_heuristic?)
+	local h = self.h_ray
+	h.to = to
+	h.from = self.camera.CFrame.Position
+	h.filter_heuristic = filter
+	
+	return h:heuristic_invoke()
 end
 
 module.enums = {
