13cdaab70b0689a94b501fb6db5c03f1ec7a779f
CHL_a <48702697+CHL-a@users.noreply.github.com>
2024-10-10 10:12:31 -0700

    Update Status.lua

--- Stuff/RobloxObjects/Status.lua
+++ Stuff/RobloxObjects/Status.lua
@@ -4,22 +4,23 @@ local Objects = script.Parent
 local Destructable = require(Objects["@CHL/Destructable"])
 local Object = require(Objects.Object)
 local Class = require(Objects.Class)
+local Dash = require(Objects["@CHL/DashSingular"])
 
 export type object<A> = {
 	name: string;
 	active: boolean;
-	host: A;
-	toggle: (self: object<A>, isActive: boolean?) -> nil;
-} & Class.subclass<Object.object>
-  & Destructable.object
+	host: A?;
+	toggle: (self: object<A>, isActive: boolean?) -> ();
+	clone: (self: object<A>) -> object<A>
+} & Object.object_inheritance
 
 --// MAIN
 local module = {}
 
 disguise = require(Objects.LuaUTypes).disguise
 
-function module.new<A>(host: A): object<A>
-	local self: object<A> = Object.new():__inherit(module)
+function module.new<A>(host: A?): object<A>
+	local self: object<A> = Object.from.class(module)
 	
 	self.active = false;
 	self.host = host
@@ -31,17 +32,31 @@ function module.new<A>(host: A): object<A>
 	return self
 end
 
-module.destroy = function<A>(self: object<A>)
-	self:assertDestruction()
+function module.destroy<A>(self: object<A>)
+	if self.isDestroyed then return end
 	
 	self:toggle(false)
 	
 	self.isDestroyed = true
 end
 
-module.toggle = Class.abstractMethod
+function module.clone<A>(self: object<A>)
+	local c: object<A> = disguise(Dash.last(self.__supers)).new(self.host)
+	
+	return c
+end
+
+function module.toggle<A>(self: object<A>, b: boolean)
+	if b == nil then
+		b = not self.active
+	end
+	
+	if b == self.active then return false;end
+	
+	self.active = b
+end
+
 module.assertDestruction = Destructable.assertDestruction
-module.__index = module
-module.className = '@CHL/Status'
+Class.makeProperClass(module, '@CHL/Status')
 
 return module
