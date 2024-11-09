860b7421e3f701e3ddbbb13180aae8a714417ce8
CHL_a <48702697+CHL-a@users.noreply.github.com>
2024-11-08 20:09:35 -0800

    Update CFrameUtils.lua

--- Stuff/RobloxObjects/CFrameUtils.lua
+++ Stuff/RobloxObjects/CFrameUtils.lua
@@ -13,4 +13,9 @@ function module.from.degrees_vector(o :Vector3): CFrame
 	return module.from.degrees_orientation(o.X, o.Y, o.Z)
 end
 
+function module.from.roblox_pack_1(p: {number}, o: {number}): CFrame
+	return CFrame.new(unpack(p)) *
+		module.from.degrees_orientation(unpack(o))
+end
+
 return module
